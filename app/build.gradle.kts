75a46114a0a7101248737eebb2551c9f1e408bfb
coolsharp <coolsharp@gmail.com>
2020-11-10 03:43:30 +0900

    gradle kts convert

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.2")
+
+    defaultConfig {
+        applicationId("com.coolsharp.dagger2")
+        minSdkVersion(16)
+        targetSdkVersion(30)
+        versionCode(1)
+        versionName("1.0")
+
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+    }
+
+    buildTypes {
+        named("release"){
+            minifyEnabled(false)
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        val options = this as org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+        options.jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("com.google.android.material:material:1.2.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.google.dagger:dagger-android:2.27")
+    implementation("com.google.dagger:dagger-android-support:2.27")
+    annotationProcessor("com.google.dagger:dagger-android-processor:2.27")
+    annotationProcessor("com.google.dagger:dagger-compiler:2.27")
+    testImplementation("junit:junit:4.13.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
